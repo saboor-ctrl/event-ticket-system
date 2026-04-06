8bcf3dc8e793f37383f683717a4783bfe1a71d1a
saboor-ctrl <saboorfatima223@gmail.com>
2026-04-06 16:53:47 -0400

    added screenings, handler, and form files

---
+++ screenings.php
@@ -0,0 +1,26 @@
+<?php
+
+function sts_get_screenings() {
+    return [
+        'wd' => [
+            'name' => 'Where Darkness Dwells',
+            'date' => 'Apr 10, 7PM',
+            'location' => 'Hall A'
+        ],
+        'kc' => [
+            'name' => 'Killing of Connor',
+            'date' => 'Apr 11, 6PM',
+            'location' => 'Hall B'
+        ],
+        'sf1' => [
+            'name' => 'Short Film Showcase 1',
+            'date' => 'Apr 12, 5PM',
+            'location' => 'Hall C'
+        ],
+        'sf2' => [
+            'name' => 'Short Film Showcase 2',
+            'date' => 'Apr 13, 4PM',
+            'location' => 'Hall D'
+        ],
+    ];
+}
